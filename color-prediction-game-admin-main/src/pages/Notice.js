7525ae5bb89bd68acf512eb99d89ab4dabd7c5a3
manish-mittal01 <mittalmanish2000@gmail.com>
2023-03-08 18:20:03 +0530

    final touch

--- color-prediction-game-admin-main/src/pages/Notice.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Notice = () => (
-    <>
-    <h1>Notice</h1>
-    </>
-  )
-
-export default Notice
--- color-prediction-game-admin-main/src/pages/Recharge.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Recharge = () => (
-  <>
-<h1>Recharge</h1>
-  </>
-  )
-
-export default Recharge
