dc1d9cc8e39a5a6a21d5c1ecf50a08f7d52a12b8
Jatin <getjatin5120@gmail.com>
2023-03-11 19:21:19 +0530

    feat: period result and not current session in bet history

--- backend/admin/services/betService.js
+++ backend/admin/services/betService.js
@@ -1,21 +1,48 @@
 const { ResponseService } = require("../../common/responseService");
+const {
+  SessionController,
+} = require("../../user/controllers/sessionController");
 const betModel = require("../../user/Models/betModel");
+const PeriodModel = require("../../user/Models/PeriodModel");
 const UserModel = require("../../user/Models/UserModel");
 
 class AdminBetService {
   static async getBets(req, res) {
     var date = new Date();
+    const currentIds = (await SessionController.getCurrentSession()).map(
+      (e) => e.periodId
+    );
     date.setDate(date.getDate() - 3);
     const allBets = await betModel
-      .find({ createdAt: { $gte: date } })
+      .find({ createdAt: { $gte: date }, periodId: { $nin: currentIds } })
       .sort({ _id: -1 });
 
     const data = [];
+    const periods = [];
+
+    async function getPeriod(periodId) {
+      var period;
+      if (!periods.includes(bet.periodId)) {
+        period = await PeriodModel.findOne({ periodId: periodId });
+        periods.push(period);
+      } else {
+        period = periods.find((e) => e.periodId == periodId);
+      }
+      return period;
+    }
 
     for await (var bet of allBets) {
       const user = await UserModel.findOne({ userId: bet.userId });
-      //   const mobile = user.mobile;
-      data.push({ ...bet._doc, mobile: user.mobile });
+      const period = await getPeriod(bet.periodId);
+      const resultColor = period.resultColor;
+      const resultNumber = period.resultNumber;
+
+      data.push({
+        ...bet._doc,
+        mobile: user.mobile,
+        resultColor,
+        resultNumber,
+      });
     }
 
     return ResponseService.success(res, "Bets found", data);
