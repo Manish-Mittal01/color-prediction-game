0db51354e4060fb2128352ed1418462eff6bd770
Jatin <getjatin5120@gmail.com>
2023-03-11 18:40:45 +0530

    feat: mobile added in bet history

--- backend/admin/services/betService.js
+++ backend/admin/services/betService.js
@@ -1,14 +1,24 @@
 const { ResponseService } = require("../../common/responseService");
 const betModel = require("../../user/Models/betModel");
+const UserModel = require("../../user/Models/UserModel");
 
 class AdminBetService {
   static async getBets(req, res) {
     var date = new Date();
     date.setDate(date.getDate() - 3);
-    const allBets = await betModel.find({ createdAt: { $gte: date } });
-    console.log(`${JSON.stringify(allBets)}`);
+    const allBets = await betModel
+      .find({ createdAt: { $gte: date } })
+      .sort({ _id: -1 });
 
-    return ResponseService.success(res, "Bets found", allBets);
+    const data = [];
+
+    for await (var bet of allBets) {
+      const user = await UserModel.findOne({ userId: bet.userId });
+      //   const mobile = user.mobile;
+      data.push({ ...bet._doc, mobile: user.mobile });
+    }
+
+    return ResponseService.success(res, "Bets found", data);
   }
 }
 
